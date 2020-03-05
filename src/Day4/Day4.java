24e4539c88d07802848640412ea72fb80f64d600
Student1 <Student1@StudentPC1>
2020-03-05 11:58:16 +0300

    Start day 4

--- src/Day4/Day4.java
+++ src/Day4/Day4.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Day4;
 
-/**
- *
- * @author Student1
- */
 public class Day4 {
-    
+
+    public static void main(String[] args) {
+
+        try {
+            try {
+                double ff = -10. / 0;
+                System.out.println(ff + " " + (int) ff);
+                int ii = 10 / 0;
+            } catch (ArithmeticException e) {
+                System.out.println(e);
+                throw new Throwable();
+            } finally {
+                System.out.println("finally");
+            }
+        } catch (Throwable e) {
+            System.out.println(e);
+        }
+
+    }
+
 }
